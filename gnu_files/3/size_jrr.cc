5c4e4f0d9f19a5eb6ede858d38c8c4087bcc42d9
raiseking <jinranran1990910@163.com>
2014-09-13 16:18:38 +0800

    0913

---
+++ gnu_files/3/size_jrr.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+
+ using std::cin;
+ using std::cout;
+ using std::endl;
+ using std::string;
+ int main()
+ {
+
+	string line("hello world");
+	int count = line.size();
+	cout << "The size of string line is:" << count << endl;
+
+	return 0;
+ }
